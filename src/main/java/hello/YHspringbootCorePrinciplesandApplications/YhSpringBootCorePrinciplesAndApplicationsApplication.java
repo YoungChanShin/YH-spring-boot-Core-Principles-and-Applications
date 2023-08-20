2358187709e2cc2bf7836d3a9888abed491858d8
YoungChan Mac Mini <youngchan@YoungChans-Mac-mini.local>
2023-08-20 19:28:54 +0900

    make project

---
+++ src/main/java/hello/YHspringbootCorePrinciplesandApplications/YhSpringBootCorePrinciplesAndApplicationsApplication.java
@@ -0,0 +1,13 @@
+package hello.YHspringbootCorePrinciplesandApplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhSpringBootCorePrinciplesAndApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YhSpringBootCorePrinciplesAndApplicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/YHspringbootCorePrinciplesandApplications/YhSpringBootCorePrinciplesAndApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.YHspringbootCorePrinciplesandApplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhSpringBootCorePrinciplesAndApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
